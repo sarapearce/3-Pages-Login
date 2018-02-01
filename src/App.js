47068256144c06d22cbae7ab1d1c6626965b15cf
Sara <sarapearce3.14@gmail.com>
2018-02-01 13:51:32 -0600

    added login, error handling, form template in App.js

--- src/App.js
+++ src/App.js
@@ -66,21 +66,6 @@ class App extends Component {
   </div>
   </div>
 
-
-
-
-
-
-
-        // <form onSubmit={this.handleSubmit}>
-        //   <label>
-        //     Name:
-        //     <input type="text" value={this.state.value} onChange={this.handleChange} />
-        //   </label>
-        //   <input type="submit" value="Submit" />
-        // </form>
-        </div>
-      </div>
 );
 }
 }
